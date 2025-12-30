3ca05160035519227f9ae5fafe111537f5233c42
Dip Ganguli <webdip@Dips-MacBook-Air.local>
2025-12-30 21:02:48 +0530

    namespace fix

--- app/Models/NovelChapterModel.php
+++ app/Models/NovelChapterModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class novelChapterModel extends Model
+class NovelChapterModel extends Model
 {
     protected $table = 'novel_chapters';
 
